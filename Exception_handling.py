33709969620726c00f4e51181e13c2a007b1d1f2
Nazim Ahmad <nazimahmad@Nazims-Air.mshome.net>
2024-12-24 13:28:51 +0530

    [nazimahmad]update: FileHandling and ExceptionHandling file are updated

--- Exception_handling.py
+++ Exception_handling.py
@@ -1,3 +1,27 @@
+# Handling exception
+try:
+    x = 10
+    y = 0
+    result = x / y
+except ZeroDivisionError:
+    print("Cannot divided by zero!")
+
+
+
+# Handling multiple exception
+'''
+try:
+    num = int(input("Enter a number : "))
+    result = 10 / num
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+except ValueError:
+    print("Error: Please enter a valid number.")
+'''
+
+
+# Handling with else
+'''
 try:
     num = int(input("Enter a number: "))
     result = 10 / num
@@ -6,6 +30,44 @@ except ZeroDivisionError:
 except ValueError:
     print("Error: Please enter a valid number.")
 else:
-    print(f"Result: {result}")
+    print(f"Result: {result}")      # Else will only print when the try block doesn't through any error
+'''
+
+
+
+
+# handling with finally block
+'''
+try:
+    x = 10
+    y = 0
+    result = x / y
+except ZeroDivisionError:
+    print("Cannot divided by zero!")
 finally:
-    print("This will always run, regardless of exceptions.")
+    print("Code execution is over")
+'''
+
+
+# Raise / custom exception
+'''
+try:
+    x = int(input("Enter a number: "))
+    if x < 0:
+        raise ValueError("Number cannot be negative.")
+except ValueError as e:
+    print(f"Error: {e}")
+'''
+
+
+
+# Catching multiple error in single exception
+
+try:
+    # Code that may raise multiple types of exceptions
+    value = int(input("Enter a number: "))
+    result = 10 / value
+except (ValueError, ZeroDivisionError) as e:
+    # Handle all specified exceptions
+    print(f"Error: {e}")
+
--- FileHandling.py
+++ FileHandling.py
@@ -3,6 +3,7 @@ f = open('demo.txt')
 print(f.read())
 f.close()
 
+print("**********************************************")
 
 f = open('demo.txt')
 print(f.read(5))    # Read only 5 characters from the file
@@ -11,13 +12,95 @@ f.close()
 
 f = open('demo.txt')
 print(f.read())
+f.close()
 
 
 
-# File appeding
-f = open('demo.txt','a')        # a is for append in file
-f.write("\nThis line is added")   # to write the content in the file
+# File handling using (with). Using with operator file automatically closed
 
-f = open('demo.txt')
+with open('demo.txt','w') as f:     # 'w' operator symbol will overwrite the content inside the file
+    f.write("I am first line\n")
+    f.write("I am also newline of this file.\n")
+
+
+
+with open('demo.txt','a') as f:
+    f.write("Third line is added.\n")
+
+
+
+# creating a new file with other mode
+with open('example.txt','w+') as f:
+    content = f.read()
+    f.write("I am the new file\n")
+
+
+with open('example.txt','r+') as f:
+    content = f.read()
+    print(content)
+
+
+print("***************************************")
+
+
+with open('example.txt','w+') as f:
+    f.write("New first is added")
+    f.seek(0)        # seek() function will move the pointer to whatever the index we have given
+    content = f.read()
+    print(content)
+
+
+
+# readline and readlines
+with open('demo.txt','r') as f:
+    print(f.readline())     # read one line
+    print(f.readlines())    # read all lines into a list
+
+
+
+# tell()
+with open('demo.txt','r') as f:
+    print(f.tell())     # current position of pointer
+
+
+
+
+
+# creating a new file
+'''
+with open('new_textFile.txt','x') as f:
+    f.write("This file is created using x mode")
+
+f = open("new_textFile.txt",'r')
 print(f.read())
 
+'''
+
+
+
+
+# Deleting a file
+import os
+if os.path.exists('Text.txt'):
+    os.remove('Text.txt')
+else:
+    print("File does not exist")
+
+
+
+# Renaming a file
+import os
+os.rename('new_textFile.txt','Text.txt')
+
+
+
+
+# Reading CSV file
+'''
+import csv
+
+with open('data.csv','r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+'''
